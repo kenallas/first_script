154a16157f6de27fe40e3deef8b95946a969aaf4
kenallas <67353178+kenallas@users.noreply.github.com>
2022-07-16 13:27:33 +0200

    Add files via upload

--- main_project.js
+++ main_project.js
@@ -1,4 +1,6 @@
 alert("Welcom to a Game where you play vijf ronds, are you ready!");
+let compWin = 0;
+let playerWin = 0;
 function computerPlay() {
     let computerSel = ["rock", "paper", "scissors"];
     return computerSel[Math.floor(Math.random() *3)];
@@ -10,18 +12,23 @@ for (let i = 1; i <= 5; i++) {
     if (playerSelection == computerSelection) 
     {
      console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, you win, its the same!`);
+     let playerWin = playerWin++;
     } 
     else if (playerSelection < computerSelection) 
     {
     console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, the computer wins!`);
+    let compWin = compWin++;
     } 
     else if (playerSelection > computerSelection)
     {
     console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, you win!`);
+    let playerWin = playerWin++;
     }
-    else if (playerSelection != computerSel)
-    {
-    console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, please fill in a valid value`);
-    } 
+}
+if (playerWin >= compWin) {
+    console.log("In total you win!");
+}
+else {
+    console.log("Sorry in total you lose!");
 }
 console.log("The end of the game!");
