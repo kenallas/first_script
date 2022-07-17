91499b8eee3a6fc0e111dd64c2c1e2d6d5444b49
kenallas <67353178+kenallas@users.noreply.github.com>
2022-07-17 11:25:34 +0200

    Add files via upload

--- main_project.js
+++ main_project.js
@@ -1,34 +1,58 @@
-alert("Welcom to a Game where you play vijf ronds, are you ready!");
+alert("Welcom to a Game where you play vijf ronds, are you ready?!");
 let compWin = 0;
 let playerWin = 0;
 function computerPlay() {
     let computerSel = ["rock", "paper", "scissors"];
     return computerSel[Math.floor(Math.random() *3)];
 }
+function testAnswer() 
+{
+    if (playerSelection = "rock") {
+    return true;
+    }
+    else if (playerSelection = "paper") {
+    return true;
+    }
+    else if (playerSelection = "scissors") {
+    return true;
+    }
+    else {
+    return false;
+    }
+    return false;
+}
 for (let i = 1; i <= 5; i++) {
-    var answer = prompt(`Fill in Rock, Paper or Scissors please?: `, `round ${i}`);
-    var playerSelection = answer.toLowerCase();
+    let answer = prompt(`Fill in Rock, Paper or Scissors please?: `, `round ${i}`);
+    let playerSelection = answer.toLowerCase();
+    let goodAnswer = testAnswer();
     let computerSelection = computerPlay ();
-    if (playerSelection == computerSelection) 
+if (goodAnswer == true) {
+    if (playerSelection === computerSelection) 
     {
-     console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, you win, its the same!`);
-     let playerWin = playerWin++;
+     console.log(`The computer says ${computerSelection}, and you say ${answer}, you win, its the same!`);
+     playerWin++;
     } 
     else if (playerSelection < computerSelection) 
     {
-    console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, the computer wins!`);
-    let compWin = compWin++;
+    console.log(`The computer says ${computerSelection}, and you say ${answer}, the computer wins!`);
+    compWin++;
     } 
     else if (playerSelection > computerSelection)
     {
-    console.log(`The computer says ${computerSelection}, and you say ${playerSelection}, you win!`);
-    let playerWin = playerWin++;
+    console.log(`The computer says ${computerSelection}, and you say ${answer}, you win!`);
+    playerWin++;
     }
+    }
+else if (goodAnswer == false) {
+    compWin++;
+    alert(`The computer says ${computerSelection}, and you say ${answer}, your input is not wright, you lose this round!`);
+    let answer = prompt(`Please fill in the form wright and fill in Rock, Paper or Scissors please?: `, `round ${i}`);
 }
-if (playerWin >= compWin) {
-    console.log("In total you win!");
+    }
+if (playerWin > compWin) {
+    console.log(`In total you win with ${playerWin} to ${compWin}`);
 }
 else {
-    console.log("Sorry in total you lose!");
+    console.log(`You lose with ${compWin} to ${playerWin}`);
 }
 console.log("The end of the game!");
