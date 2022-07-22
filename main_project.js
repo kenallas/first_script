cf816b7b23eadd00e29c9658677b0cdc5b2eece2
kenallas <67353178+kenallas@users.noreply.github.com>
2022-07-22 13:17:02 +0200

    Add files via upload

--- main_project.js
+++ main_project.js
@@ -1,58 +1,69 @@
-alert("Welcom to a Game where you play vijf ronds, are you ready?!");
+alert("Welcom to a Game where you play five ronds, are you ready?!");
 let compWin = 0;
 let playerWin = 0;
-function computerPlay() {
-    let computerSel = ["rock", "paper", "scissors"];
+const computerSel = ["rock", "paper", "scissors"];
+for (let i = 1; i <= 5; i++) 
+{
+    let answer = prompt(`Fill in Rock, Paper or Scissors please?: `, `${i}`);
+    if ((answer === null) || (answer === undefined))
+    {
+        break;
+    }
+    let computerSelection = computerPlay ();
+    while (((answer.length) >= 4) && ((answer.length) <= 8) && (testinput(answer) === true)) 
+    {
+        if (answer.toLowerCase() === computerSelection) 
+        {
+        console.log (`The computer says ${computerSelection}, and you say ${answer}, you win, it's a draw!`);
+        playerWin++;
+        break;
+        } 
+        else if (answer.toLowerCase() < computerSelection) 
+        {
+        console.log (`The computer says ${computerSelection}, and you say ${answer}, the computer wins!`);
+        compWin++;
+        break;
+        } 
+        else if (answer.toLowerCase() > computerSelection)
+        {
+        console.log (`The computer says ${computerSelection}, and you say ${answer}, you win!`);
+        playerWin++;
+        break;
+        }
+    } 
+    if ((answer.length) < 4)
+    {
+        compWin++;
+        alert (`The computer says ${computerSelection}, and you say ${answer}, your input is not wright, you lose this round!`);
+    } 
+    else if ((answer.length) > 8) 
+    {
+        compWin++;
+        alert (`The computer says ${computerSelection}, and you say ${answer}, your input is not wright, you lose this round!`);
+    }
+}
+function testinput(answer)
+{
+    switch (answer)
+    {
+        case computerSel[0]:
+        return true;
+        case computerSel[1]:
+        return true;
+        case computerSel[2]:
+        return true;
+    }
+    return false;
+}
+function computerPlay() 
+{
     return computerSel[Math.floor(Math.random() *3)];
 }
-function testAnswer() 
+if (playerWin > compWin) 
 {
-    if (playerSelection = "rock") {
-    return true;
-    }
-    else if (playerSelection = "paper") {
-    return true;
-    }
-    else if (playerSelection = "scissors") {
-    return true;
-    }
-    else {
-    return false;
-    }
-    return false;
-}
-for (let i = 1; i <= 5; i++) {
-    let answer = prompt(`Fill in Rock, Paper or Scissors please?: `, `round ${i}`);
-    let playerSelection = answer.toLowerCase();
-    let goodAnswer = testAnswer();
-    let computerSelection = computerPlay ();
-if (goodAnswer == true) {
-    if (playerSelection === computerSelection) 
-    {
-     console.log(`The computer says ${computerSelection}, and you say ${answer}, you win, its the same!`);
-     playerWin++;
-    } 
-    else if (playerSelection < computerSelection) 
-    {
-    console.log(`The computer says ${computerSelection}, and you say ${answer}, the computer wins!`);
-    compWin++;
-    } 
-    else if (playerSelection > computerSelection)
-    {
-    console.log(`The computer says ${computerSelection}, and you say ${answer}, you win!`);
-    playerWin++;
-    }
-    }
-else if (goodAnswer == false) {
-    compWin++;
-    alert(`The computer says ${computerSelection}, and you say ${answer}, your input is not wright, you lose this round!`);
-    let answer = prompt(`Please fill in the form wright and fill in Rock, Paper or Scissors please?: `, `round ${i}`);
-}
-    }
-if (playerWin > compWin) {
-    console.log(`In total you win with ${playerWin} to ${compWin}`);
+    console.log (`In total you win with ${playerWin} to ${compWin}`);
 }
 else {
-    console.log(`You lose with ${compWin} to ${playerWin}`);
+    console.log (`You lose with ${compWin} to ${playerWin}`);
 }
-console.log("The end of the game!");
+console.log ("This is the end of the game!");
